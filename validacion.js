c4a408346e8bbce87a7d728b5fc0733e92876e66
Luciana Romina Schmidt <luciana.rschmidt@gmail.com>
2023-07-08 22:11:14 -0300

    Estilizaciones para responsividad

--- validacion.js
+++ validacion.js
@@ -1,1 +1,43 @@
-//Haz tú validación en javascript acá
+function validarFormulario() {
+    var nombre = document.forms["formulario"]["nombre"].value;
+    var email = document.forms["formulario"]["email"].value;
+    var asunto = document.forms["formulario"]["asunto"].value;
+    var mensaje = document.forms["formulario"]["mensaje"].value;
+
+    var mensajesError = document.getElementsByClassName("error");
+        for (var i = 0; i < mensajesError.length; i++) {
+            mensajesError[i].style.display = "none";
+        }
+
+        if (nombre == "") {
+            mostrarError("nombre", "Por favor, ingresa tu nombre.");
+            return false;
+        }
+
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailRegex.test(email)) {
+            mostrarError("email", "Por favor, ingresa un correo electrónico válido.");
+            return false;
+        }
+
+ 
+        if (asunto == "") {
+            mostrarError("asunto", "Por favor, ingresa un asunto.");
+            return false;
+        }
+
+        if (mensaje == "") {
+            mostrarError("mensaje", "Por favor, ingresa un mensaje.");
+            return false;
+        }
+
+           
+}
+
+function mostrarError(campo, mensaje) {
+    var elementoCampo = document.getElementById(campo);
+    var mensajeError = document.createElement("span");
+        mensajeError.className = "error";
+        mensajeError.innerHTML = mensaje;
+        elementoCampo.parentNode.insertBefore(mensajeError, elementoCampo.nextSibling);
+}
