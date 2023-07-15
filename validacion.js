b0bf2d42d990d11a37f70865b03e9d981824697e
Luciana Romina Schmidt <luciana.rschmidt@gmail.com>
2023-07-14 21:08:42 -0300

    Arreglos

--- validacion.js
+++ validacion.js
@@ -1,65 +1,70 @@
-// Obtener referencia al formulario y los campos de entrada
 const formulario = document.getElementById('formulario');
 const nombreInput = document.getElementById('nombre');
 const emailInput = document.getElementById('email');
 const asuntoInput = document.getElementById('asunto');
 const mensajeInput = document.getElementById('mensaje');
+const errorSpan = document.getElementById('demo');
 
-// Funciones para mostrar y ocultar mensajes de error
-function mostrarError(elemento, mensaje) {
-  const errorSpan = elemento.nextElementSibling;
+function mostrarError(mensaje) {
   errorSpan.textContent = mensaje;
 }
 
-function ocultarError(elemento) {
-  const errorSpan = elemento.nextElementSibling;
+function ocultarError() {
   errorSpan.textContent = '';
 }
 
-// Función para validar el formulario
 function validarFormulario(event) {
-  event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+  event.preventDefault();
 
-  // Validar campo nombre
-  if (nombreInput.value.trim() === '' || nombreInput.value.length > 50) {
-    mostrarError(nombreInput, 'Por favor, ingresa un nombre válido (máximo 50 caracteres).');
+  ocultarError();
+
+  if (nombreInput.value.trim() === '') {
+    mostrarError('Por favor, ingresa tu nombre.');
     return;
-  } else {
-    ocultarError(nombreInput);
   }
 
-  // Validar campo email
+  if (nombreInput.value.length > 50) {
+    mostrarError('El nombre no puede tener más de 50 caracteres.');
+    return;
+  }
+
+  if (emailInput.value.trim() === '') {
+    mostrarError('Por favor, ingresa tu dirección de correo electrónico.');
+    return;
+  }
+
   const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
-  if (emailInput.value.trim() === '' || !emailRegex.test(emailInput.value)) {
-    mostrarError(emailInput, 'Por favor, ingresa una dirección de correo electrónico válida.');
+  if (!emailRegex.test(emailInput.value)) {
+    mostrarError('Por favor, ingresa una dirección de correo electrónico válida.');
     return;
-  } else {
-    ocultarError(emailInput);
   }
 
-  // Validar campo asunto
-  if (asuntoInput.value.trim() === '' || asuntoInput.value.length > 50) {
-    mostrarError(asuntoInput, 'Por favor, ingresa un asunto válido (máximo 50 caracteres).');
+  if (asuntoInput.value.trim() === '') {
+    mostrarError('Por favor, ingresa el asunto del mensaje.');
     return;
-  } else {
-    ocultarError(asuntoInput);
   }
 
-  // Validar campo mensaje
-  if (mensajeInput.value.trim() === '' || mensajeInput.value.length > 300) {
-    mostrarError(mensajeInput, 'Por favor, ingresa un mensaje válido (máximo 300 caracteres).');
+  if (asuntoInput.value.length > 50) {
+    mostrarError('El asunto no puede tener más de 50 caracteres.');
     return;
-  } else {
-    ocultarError(mensajeInput);
   }
 
-  // Si todas las validaciones pasan, enviar el formulario
+  if (mensajeInput.value.trim() === '') {
+    mostrarError('Por favor, ingresa tu mensaje.');
+    return;
+  }
+
+  if (mensajeInput.value.length > 300) {
+    mostrarError('El mensaje no puede tener más de 300 caracteres.');
+    return;
+  }
+  
   formulario.submit();
 }
 
-// Agregar evento de escucha al botón de envío
 const enviarBoton = document.querySelector('.formcontato__botao');
 enviarBoton.addEventListener('click', validarFormulario);
+
 
 
 
